b337c2a6da0e98455888475ef0614495ea282097
Mustafailhann <2103022207@student.beykent.edu.tr>
2026-07-14 10:32:15 +0300

    fix: hardcode production API URL for Vercel

--- apps/client/lib/core/config/environment_config.dart
+++ apps/client/lib/core/config/environment_config.dart
@@ -2,10 +2,5 @@ import 'package:flutter/foundation.dart';
 
 class EnvironmentConfig {
   // Can be expanded to read from .env or flavors later
-  static const String baseUrl = String.fromEnvironment(
-    'API_BASE_URL',
-    defaultValue: kReleaseMode 
-        ? 'https://reality-lens-api.onrender.com/api' 
-        : (kIsWeb ? 'http://localhost:5128/api' : 'http://10.0.2.2:5128/api'),
-  );
+  static const String baseUrl = 'https://reality-lens-api.onrender.com/api';
 }
